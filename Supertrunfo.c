5e3113a79ea124293388cef7cbeea47755abd0b5
Gasperin123 <vinigaspa@gmail.com>
2025-08-11 20:47:28 -0300

    Update Supertrunfo.c
    Inclusão dos calculos de densidade populacional e PIB per capita

--- Supertrunfo.c
+++ Supertrunfo.c
@@ -2,20 +2,22 @@
 
 int main() {
     char estado1, estado2;
-    char cod_carta1[3], cod_carta2[3];
+    char cod_carta1[4], cod_carta2[4];
     char cidade1[25], cidade2[25];
     int pop1, pop2;
     float area1, area2;
     float PIB1, PIB2;
     int num_pt_tur1, num_pt_tur2;
+    float densidade1, densidade2;
+    float PIB_PC1, PIB_PC2;
 
     printf("Insira os dados da primeira carta: \n");
     printf("\nEstado: ");
     scanf(" %c", &estado1);
     printf("\nCodigo da carta: ");
-    scanf(" %s", cod_carta1);
+    scanf("%s", cod_carta1);
     printf ("\nCidade: ");
-    scanf(" %s", cidade1);
+    scanf("%s", cidade1);
     printf("\npopulacao: ");
     scanf("%d", &pop1);
     printf("\nArea do municipio: ");
@@ -24,14 +26,16 @@ int main() {
     scanf("%f", &PIB1);
     printf("\nNumero de pontos turisticos do municipio: ");
     scanf("%d", &num_pt_tur1);
+    densidade1 = pop1/area1;
+    PIB_PC1= PIB1/pop1;
 
     printf("Insira os dados da segundaa carta: \n");
     printf("\nEstado: ");
     scanf(" %c", &estado2);
     printf("\nCodigo da carta: ");
-    scanf(" %s", cod_carta2);
+    scanf("%s", cod_carta2);
     printf ("\nCidade: ");
-    scanf(" %s", cidade2);
+    scanf("%s", cidade2);
     printf("\npopulacao: ");
     scanf("%d", &pop2);
     printf("\nArea do municipio: ");
@@ -40,9 +44,12 @@ int main() {
     scanf("%f", &PIB2);
     printf("\nNumero de pontos turísticos do município: ");
     scanf("%d", &num_pt_tur2);
+    densidade2 = pop2/area2;
+    PIB_PC2= PIB2/pop2;
 
-    printf("\n\nCarta 1:\nEstado: %c\nCodigo: %s\nNome da cidade: %s\nPopulacao: %d\nArea: %f\nPIB: %f\nNumero de pontos turisticos: %d\n\n", estado1, cod_carta1, cidade1, pop1, area1, PIB1, num_pt_tur1);
+    printf("\n\nCarta 1:\nEstado: %c\nCodigo: %s\nNome da cidade: %s\nPopulacao: %d\nArea: %f KM2\nPIB: %f bilhoes de reais\nNumero de pontos turisticos: %d\nDensidade populacional: %f hab/KM2\nPIB per capita:%f reais\n\n", estado1, cod_carta1, cidade1, pop1, area1, PIB1, num_pt_tur1, densidade1, PIB_PC1);
     
-    printf("\n\nCarta 2:\nEstado: %c\nCodigo: %s\nNome da cidade: %s\nPopulacao: %d\nÁrea: %f\nPIB: %f\nNumero de pontos turisticos: %d\n\n", estado2, cod_carta2, cidade2, pop2, area2, PIB2, num_pt_tur2);
+    printf("\n\nCarta 2:\nEstado: %c\nCodigo: %s\nNome da cidade: %s\nPopulacao: %d\nArea: %f KM2\nPIB: %f bilhoes de reais\nNumero de pontos turisticos: %d\nDensidade populacional: %f hab/KM2\nPIB per capita:%f reais\n\n", estado2, cod_carta2, cidade2, pop2, area2, PIB2, num_pt_tur2, densidade2, PIB_PC2);
+    
     return 0;
 }
